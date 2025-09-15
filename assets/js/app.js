1701f33ac26a5d3653b4f182daab6ad6e73402a1
Ollaberganov-Mirzoxid <ollaberganovmirzoxid@gmail.com>
2025-09-15 10:57:42 +0500

    xatolarni tuzatish 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,7 @@ new Chart(ctx1, {
         }]
     }
 });
+
+document.getElementById("sidebarToggle").addEventListener("click", function () {
+  document.getElementById("sidebar").classList.toggle("active");
+});
